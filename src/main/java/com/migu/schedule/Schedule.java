8ffb152bcbd2226b6098e4dafdb4303e0c1dd6d1
李慧红 <13681998636@139.com>
2018-06-20 21:44:25 +0800

    first commit

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -4,52 +4,162 @@ package com.migu.schedule;
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
 
-import java.util.List;
+import java.util.*;
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
 
+    //定义节点注册队列
+    private  static List<Integer> nodeIds = new LinkedList<Integer>();
+    //定义任务挂起队列
+    private  static List<Integer> taskIds = new LinkedList<Integer>();
+    //定义任务信息列表
+    private  static List<TaskInfo> tasks = new ArrayList<TaskInfo>();
+    //定义任务资源消耗表
+    private  static Map<Integer,Integer> taskConsum = new HashMap<Integer, Integer>();
 
+    /**
+     * 系统初始化
+     * @return
+     */
     public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        // 清空任务列表,注册节点和任务挂起队列
+        if(tasks.size()!=0){
+            tasks.clear();
+        }
+        if(taskIds.size()!=0){
+            taskIds.clear();
+        }
+        if(nodeIds.size()!=0){
+            nodeIds.clear();
+        }
+        return ReturnCodeKeys.E001;
     }
 
-
+    /**
+     * 注册节点
+     * @param nodeId
+     * @return
+     */
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(nodeId<=0){
+            return ReturnCodeKeys.E004;
+        }
+        if(nodeIds.contains(nodeId)){
+            return ReturnCodeKeys.E005;
+        }
+        nodeIds.add(nodeId);
+        return ReturnCodeKeys.E003;
     }
 
+    /**
+     * 注销节点
+     * @param nodeId
+     * @return
+     */
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(nodeId<=0){
+            return ReturnCodeKeys.E004;
+        }
+        if(!nodeIds.contains(nodeId)){
+            return ReturnCodeKeys.E007;
+        }
+        //注销节点
+        if(tasks.size()!=0)
+        {
+            for (TaskInfo taskInfo : tasks)
+            {
+                if (taskInfo.getNodeId() == nodeId)
+                {
+                    //移到任务挂起队列
+                    taskIds.add(taskInfo.getTaskId());
+                    tasks.remove(taskInfo);
+                }
+            }
+        }
+        for(int i=nodeIds.size()-1;i>=0;i--){
+            if(nodeIds.get(i)==nodeId){
+                nodeIds.remove(i);
+            }
+        }
+        return ReturnCodeKeys.E006;
     }
 
-
+    /**
+     * 添加任务
+     * @param taskId
+     * @param consumption
+     * @return
+     */
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(taskId<=0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        if(taskIds.contains(taskId))
+        {
+            return ReturnCodeKeys.E010;
+        }
+        taskConsum.put(taskId,consumption);
+        //移到任务挂起队列
+        taskIds.add(taskId);
+        return ReturnCodeKeys.E008;
     }
 
-
+    /**
+     * 删除任务
+     * @param taskId
+     * @return
+     */
     public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(taskId<=0)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        if(!taskIds.contains(taskId))
+        {
+            return ReturnCodeKeys.E012;
+        }
+
+        for(int i=taskIds.size()-1;i>=0;i--)
+        {
+            if(taskIds.get(i)==taskId){
+                taskIds.remove(i);
+            }
+        }
+        return ReturnCodeKeys.E011;
     }
 
-
+    /**
+     * 任务调度
+     * @param threshold
+     * @return
+     */
     public int scheduleTask(int threshold) {
-        // TODO 方法未实现
+
+
         return ReturnCodeKeys.E000;
     }
 
-
+    /**
+     * 查询任务调度列表
+     * @param tasks
+     * @return
+     */
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(tasks == null || tasks.size()==0){
+            return ReturnCodeKeys.E016;
+        }
+        TreeMap<Integer,Integer> map = new TreeMap<Integer, Integer>();
+        for(TaskInfo taskInfo : tasks){
+            map.put(taskInfo.getTaskId(),taskInfo.getNodeId());
+        }
+        tasks.clear();
+        List<Map> list = new ArrayList<Map>();
+        
+        return ReturnCodeKeys.E015;
     }
 
 }
